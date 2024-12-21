43461caf3dc353c2135ff62aca118dbb7109c34c
leverimmy <leverimmy@gmail.com>
2024-12-21 17:19:22 +0800

    feat: Update code.

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # THU-Annual-Lib
+
 一年过去了，你在华子的图书馆自习过多少次？
+
+## 项目简介
+
+本项目是一个用于统计华清大学学生在图书馆的自习情况的脚本。通过模拟登录华清大学图书馆空间管理系统，获取学生在华清大学的各个图书馆的预约记录，并通过数据可视化的方式展示。
+
+![demo](./demo.png)
+
+## 使用方法
+
+### 0. 获取访问令牌
+
+首先，登录校园卡账号后，在[华清大学图书馆空间管理系统](https://seat.lib.tsinghua.edu.cn/user/index/book)获取你的访问令牌。
+
+![library](./library.png)
+
+`F12` 打开开发者工具，切换到 Network（网络）标签页，然后 `Ctrl + R` 刷新页面，找到 `` 这个请求，查看标头中的 `Cookie` 字段，其中包含了你的访问令牌。
+
+访问令牌是 `access_token=` **之后**的一串字符（不含 `access_token=`），复制下来，后面会用到。
+
+![access_token](./access_token.png)
+
+### 1. 安装依赖
+
+本项目依赖于 `requests`、`matplotlib`、`bs4` 和 `re`，请确保你的 Python 环境中已经安装了这些库。
+
+```bash
+pip install requests matplotlib bs4 re
+```
+
+### 2. 运行脚本
+
+```bash
+python main.py
+```
+
+首次运行时，请输入你的学号和访问令牌，会自动保存在 `config.json` 文件中。
+
+### 3.（可选）修改配置
+
+如果你想修改学号或者访问令牌，可以直接修改 `config.json` 文件。
+
+```json
+{
+    "userid": "你的学号",
+    "access_token": "你的访问令牌"
+}
+```
+
+## LICENSE
+
+除非另有说明，本仓库的内容采用 [CC BY-NC-SA 4.0](https://creativecommons.org/licenses/by-nc-sa/4.0/) 许可协议。在遵守许可协议的前提下，您可以自由地分享、修改本文档的内容，但不得用于商业目的。
+
+如果您认为文档的部分内容侵犯了您的合法权益，请联系项目维护者，我们会尽快删除相关内容。
---
+++ main.py
@@ -0,0 +1,121 @@
+from datetime import datetime
+import json
+import re
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import platform
+import requests
+
+
+start_date = datetime.strptime("2024-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
+end_date = datetime.strptime("2024-12-31 23:59:59", "%Y-%m-%d %H:%M:%S")
+all_data = {}
+
+
+if __name__ == "__main__":
+    # 读入账户信息
+    try:
+        with open("config.json", "r", encoding='utf-8') as f:
+            account = json.load(f)
+            userid = account["userid"]
+            access_token = account["access_token"]
+    except Exception as e:
+        print("账户信息读取失败，请重新输入")
+        idserial = input("请输入学号: ")
+        servicehall = input("请输入访问令牌: ")
+        with open("config.json", "w", encoding='utf-8') as f:
+            json.dump({"userid": userid, "access_token": access_token}, f, indent=4)
+    
+    # 发送请求，得到总页数
+    url = "https://seat.lib.tsinghua.edu.cn/user/index/book/"
+    cookie = {
+        "userid": userid,
+        "access_token": access_token
+    }
+    response = requests.post(url, cookies=cookie)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    links = soup.find_all('a')
+
+    max_page = None
+    for link in links:
+        if 'class' in link.attrs and 'end' in link['class']:
+            max_page = int(link['href'].split('p/')[1])
+            break
+
+    # 发送请求，得到所有数据
+    reservations = {}
+    for page in range(1, max_page + 1):
+        url = f"https://seat.lib.tsinghua.edu.cn/user/index/book/p/{page}"
+        cookie = {
+            "userid": userid,
+            "access_token": access_token
+        }
+        response = requests.post(url, cookies=cookie)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # 找到所有的表格行
+        rows = soup.find_all('tr')
+
+        # 遍历每一行，提取需要的信息
+        for row in rows:
+            # 找到所有的单元格
+            cols = row.find_all('td')
+            if len(cols) > 5:  # 确保行中有足够的单元格
+                reservation_id = cols[0].text.strip()
+                places = re.sub(r'【.*?】', '', cols[1].text.strip()).split('-')
+                lib = places[0]
+                floor = places[1]
+                seat = places[2].split(':')
+                section = seat[0]
+                code = seat[1]
+                starttime = cols[2].text.strip()
+                endtime = cols[3].text.strip()
+                
+                # 将信息添加到字典中
+                reservations[reservation_id] = {
+                    'lib': lib,
+                    'floor': floor,
+                    'section': section,
+                    'code': code,
+                    'starttime': starttime,
+                    'endtime': endtime
+                }
+
+    # 整理数据
+    for item in reservations.values():
+        place = item["lib"] + "-" + item["floor"] + "-" + item["section"]
+        data_date = datetime.strptime(item["starttime"], "%Y-%m-%d %H:%M:%S")
+
+        if start_date <= data_date <= end_date:
+            try:
+                if place in all_data:
+                    all_data[place] += 1
+                else:
+                    all_data[place] = 1
+            except Exception as e:
+                pass
+
+    # 输出结果
+    all_data = dict(sorted(all_data.items(), key=lambda x: x[1], reverse=False))
+    if platform.system() == "Darwin":
+        plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+    elif platform.system() == "Linux":
+        plt.rcParams['font.family'] = ['Droid Sans Fallback', 'DejaVu Sans']
+    else:
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+        
+    plt.figure(figsize=(20, 8))
+    plt.barh(list(all_data.keys()), list(all_data.values()))
+    for index, value in enumerate(list(all_data.values())):
+        plt.text(value + 0.01 * max(all_data.values()),
+                index,
+                str(value),
+                va='center')
+        
+    # plt.tight_layout()
+    plt.xlim(0, 1.2 * max(all_data.values()))
+    plt.title("华清大学自习情况")
+    plt.xlabel("自习次数（次）")
+    plt.savefig("result.png")
+    plt.show()
